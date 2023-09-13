68a2a16838c48725d35b9327d30bf412c97990f9
CharaD7 <jijakahn6@gmail.com>
2023-09-13 22:07:35 +0000

    Covered python variables

--- basics.py
+++ basics.py
@@ -1,2 +1,19 @@
 # Starting out with python programming
 print('Hello, world!')
+
+# Working with variables
+fname = 'Jija' # string
+lname = 'Kahn' # string
+age = '40' # string
+person_age = 40 # integer
+email = 'jijakahn6@gmail.com' # string
+contacts = [ 553020508, 204860341 ] # list
+contact_types = { 'Home': 204860341, 'Work': 553020508 } # dictionary
+married = False # boolean
+
+print()
+print('Fname is', fname, 'it is of type', type(fname))
+print('Person age is', person_age, 'it is of type', type(person_age))
+print('Contacts is', contacts, 'it is of type', type(contacts))
+print('Contact types is', contact_types, 'it is of type', type(contact_types))
+print('Married is', married, 'it is of type', type(married))
