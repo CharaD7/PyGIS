2710546303c38bbd7dcfc107ee3e0f9171aa15ed
CharaD7 <jijakahn6@gmail.com>
2023-09-21 22:04:03 +0000

    Added tuple variable and printed key-value pairs for dict variable

--- basics.py
+++ basics.py
@@ -8,6 +8,7 @@ age = '40' # string
 person_age = 40 # integer
 email = 'jijakahn6@gmail.com' # string
 contacts = [ 553020508, 204860341 ] # list
+telephone = ( 30553020508, 30204860341 ) # tuple
 contact_types = { 'Home': 204860341, 'Work': 553020508 } # dictionary
 married = False # boolean
 
@@ -17,3 +18,5 @@ print('Person age is', person_age, 'it is of type', type(person_age))
 print('Contacts is', contacts, 'it is of type', type(contacts))
 print('Contact types is', contact_types, 'it is of type', type(contact_types))
 print('Married is', married, 'it is of type', type(married))
+print(f'Dictionary keys => { contact_types.keys() }')
+print(f'Dictionary values => { contact_types.values() }')
