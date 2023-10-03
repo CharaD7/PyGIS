7e5c65bff945dbb71806382db5a11b1757f2f1e5
CharaD7 <jijakahn6@gmail.com>
2023-10-03 21:19:14 +0000

    Used extend and append in list

--- dtypes.py
+++ dtypes.py
@@ -8,6 +8,11 @@ print('Second item in contacts list => ', contacts[1])
 print()
 additional_contacts = [ 2553322314, 552938123, 207236168 ]
 # print('Assigning a new item to the 1st position in contacts list => ', contacts[0])
+contacts.extend(additional_contacts)
+print('View all elements in contacts list => ', contacts)
+
+contacts.append(271029899)
+print()
 print('View all elements in contacts list => ', contacts)
 
 
