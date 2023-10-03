09471af72568766248273fcb2dd1b3be7d9e0431
CharaD7 <jijakahn6@gmail.com>
2023-10-03 21:16:06 +0000

    Started out Python lists

---
+++ dtypes.py
@@ -0,0 +1,17 @@
+# List datatypes
+contacts = [ 553020508, 204860341 ] # list
+
+print()
+print('First item in contacts list => ', contacts[0])
+print('Second item in contacts list => ', contacts[1])
+
+print()
+additional_contacts = [ 2553322314, 552938123, 207236168 ]
+# print('Assigning a new item to the 1st position in contacts list => ', contacts[0])
+print('View all elements in contacts list => ', contacts)
+
+
+
+
+
+# Dictionary datatypes
